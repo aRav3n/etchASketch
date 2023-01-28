e205460641681cf61c052c7335db2a1ab5d3821b
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-28 10:16:05 +0530

    Create grid
    Grid is for sketching on and is 16 x 16

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const sketchBody = document.querySelector('#sketchBody');
 let sketchRow;
 let sketchColumn;
 
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 16; i++){
     sketchRow = document.createElement('div');
     sketchRow.classList.add('sketchRow');
-    for (j = 0; j < 4; j++){
+    for (j = 0; j < 16; j++){
         sketchColumn = document.createElement('div');
         sketchColumn.classList.add('sketchColumn');
         sketchRow.appendChild(sketchColumn);
